eaab89820495c90fa72d8ee85b0d3da212243052
davidwander <165208277+davidwander@users.noreply.github.com>
2024-07-10 21:22:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2 ");
+
+function handleClick() {
+  screen1.classList.toggle("hide");
+
+  screen2.classList.toggle("hide");
+}
